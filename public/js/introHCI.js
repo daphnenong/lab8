1564d12b52c5da73007bebd1417a1086eda3f501
daphnenong <47928132+daphnenong@users.noreply.github.com>
2021-02-24 21:20:34 -0800

    meowmeowmeoweow!!

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,8 +1,8 @@
 // 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-  initializePage();
+$(document).ready(function () {
+	initializePage();
 })
 
 /*
@@ -10,18 +10,33 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
- 	initGestures();
- 	initRSVPForm();
+	initCamera();
+	initMap();
+	initGestures();
+	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	$(function () {
+		$(".judge-img").bind("taphold", tapholdHandler);
+		function tapholdHandler(event) {
+			var targetIDPrefix = event.target.id;
+			$("#" + targetIDPrefix + "-bio").show();
+		}
+	});
 }
 
-// init RSVP form submit listener
 function initRSVPForm() {
-  // add your code here
+	$('#rsvpForm').submit(function (e) {
+		e.preventDefault();
+		var rsvpEmail = $('#rsvpEmail').val();
+		$.post('addRSVP', { rsvpEmail: rsvpEmail }, postCallback);
+	});
+
+	function postCallback(res) {
+		alert("RSVP form successfully submitted!");
+		$('#rsvpEmail').val('');
+		$('#rsvpExcited').val('');
+	}
 }
